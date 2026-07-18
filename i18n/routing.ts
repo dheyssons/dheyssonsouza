fe020c501cc4b3e4d8df575f2ec725e24ea28252
dheysson <dheyssonsousasilva@gmail.com>
2026-07-18 16:52:26 -0300

    add

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,7 @@ import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   locales: ["en", "nl", "fr", "pt"],
-  defaultLocale: "en",
+  defaultLocale: "pt",
   localePrefix: "always",
 });
 
